1bdd07a7e2b3f6e68b75af7b321adf1e96e021b8
Dmitry-Kirtadze <mr.dima.kirtadze@gmail.com>
2024-02-19 22:10:06 +0200

    MakeAnim

---
+++ Scripts/MainHero/PlayerMove.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public bool IsGround = false;
+
+    [SerializeField] private float modifySpeed;
+    [SerializeField] private float forceJump;
+    public float MoveSpeed;
+    private Animator animator;
+    private SpriteRenderer sprite;
+
+    private Rigidbody2D rb;
+    void Start()
+    {
+        animator = GameObject.Find("DayanaRender").GetComponent<Animator>();
+        sprite = GameObject.Find("DayanaRender").GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+  
+    void Update()
+    {
+        SimpleMove();
+        SimpleJump();
+
+
+
+    }
+
+
+
+    private float currentVelocity;
+    void SimpleJump()
+    {
+        if (IsGround)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                //Vector2 pos = Vector2.Lerp(transform.position.x, transform.position.y, 2f);
+                //transform.position = new Vector2(rb.velocity.x, pos);
+                Debug.Log("Abobik");
+            }
+        }
+    }
+
+
+
+
+
+
+
+    void SimpleMove()
+    {
+        MoveSpeed = Input.GetAxis("Horizontal") * modifySpeed * Time.fixedDeltaTime;
+        rb.velocity = new Vector2(MoveSpeed, 0);
+        float MoveSpeed2Anim = MoveSpeed;
+
+        if (MoveSpeed < 0)
+        {
+            MoveSpeed2Anim = MoveSpeed * -1f;
+            sprite.flipX = true;
+        }
+        else
+        {
+            sprite.flipX = false;
+        }
+        animator.SetFloat("moveSpeed", MoveSpeed2Anim);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Ground")){
+            IsGround = true;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Ground"))
+        {
+            IsGround = false;
+        }
+    }
+}
